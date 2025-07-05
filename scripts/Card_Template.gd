40019c9305871711ae6331f1af1b40541b65b44b
Kerenas <arcwoven@gmail.com>
2025-07-05 17:43:35 -0500

    changed Anomalous to Requires Shroud Blessing as it implies that the cards were lost to the shroud and require its blessing to find them.

--- scripts/Card_Template.gd
+++ scripts/Card_Template.gd
@@ -30,7 +30,7 @@ class_name Card extends Node2D
 @export var IsPlusHP: bool
 @export var PlusHPValue: int
 @export var IsProdigy: bool
-@export var Anomalous: bool
+@export var RequiresShroudBlessing: bool #denotes Developer Joke Card and Steam Inventory Drop-Only cards.
 
 signal hovered
 signal hovered_off
